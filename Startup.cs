36e6ae5a371da0fbe31f4ea16ba985e90e7e911c
Mario Torres <mariotorresdev@gmail.com>
2020-08-24 16:27:23 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -30,8 +30,7 @@ namespace server
             services.AddScoped<IFileUpload, FileUpload>();
             
             services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();           
+            services.AddServerSideBlazor();         
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
